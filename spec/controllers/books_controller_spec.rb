14088fd7f3f62e7d28e32bc83700a19f2936ea68
rhuberdeau <rhuberdeau@gmail.com>
2015-06-16 08:46:58 -0400

    Cleaning up book controller specs now that we have a FindsBook class

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -5,48 +5,25 @@ describe BooksController do
 
 	context "when there is an ASIN" do 
 		let(:asin) { 'something' }
+		before { allow(controller).to receive(:current_user) { user } }
 		
-		context "when the book is in the database" do 
+		context "when there is a book" do 
 			let!(:book) do 
 				Book.create!(:asin => asin,
 										 :title => "a title",
 										 :authors => "Fred Smith",
 										 :details_url => "http://www.example.com/a-title")
 			end
-			before { allow(controller).to receive(:current_user) { user } }
 			
 			it "calls add_book" do 
 				expect(user).to receive(:add_book).with book
 				post :create, :id => asin
 			end
 		end
-		context "when the book is not in the database" do 
-			let!(:book_attributes) do 
-				{:asin => asin,
-				 :title => "a title",
-				 :authors => "Fred Smith",
-				 :details_url => "http://www.example.com/a-title"}
-			end
-			before { allow(controller).to receive(:current_user) { user } }
-
-			context "when the book is in amazon" do 
-				it "adds the book to the user" do 
-					amazon_book = double
-					allow(amazon_book).to receive(:attributes) {book_attributes}
-					allow(AmazonBook).to receive(:find_by_asin).with(asin) {amazon_book}
-					post :create, :id => asin 
-					expect(user.books.count).to eql 1
-					expect(user.books.first.title).to eql "a title"
-				end
-			end
-
-			context "when the book is not in amazon" do 
-				it "does not add the book to the user" do 
-					allow(Book).to receive(:find_by_asin).with(asin) { nil }
-					allow(AmazonBook).to receive(:find_by_asin).with(asin) { nil }
-					post :create, :id => asin
-					expect(user.books).to be_empty
-				end
+		context "when there isn't a book" do 
+			it "does not add the book to the user" do 
+				allow(FindsBook).to receive(:find_book).with(asin) {nil}
+				expect(user.books).to be_empty
 			end
 		end
 	end
